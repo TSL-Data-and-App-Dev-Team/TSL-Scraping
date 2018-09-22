f5a31ac5f43356c393c8003eb9d07026182df5d9
santoshnpl54 <santoshnpl54@gmail.com>
2018-09-22 12:05:22 -0500

    This file will display the contents in list format

---
+++ content_in_list.py
@@ -0,0 +1,23 @@
+import requests
+r= requests.get('http://www.tsl.news')
+
+#using BeautifulSoup framework
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(r.text, 'html.parser')
+
+#fields= soup.findAll('article', attrs={'class':'article-preview'})
+
+
+#for link in fields:
+for link in soup.findAll('a'):
+    #stores all the lines with starting a in link
+    
+    for content in link.contents:
+       #link.contents has all the contents of the field starting with a
+        try:
+            #strips the leftside  and rightside space from the words
+            print(content.strip())
+        except:
+            print(' ')
+
+
